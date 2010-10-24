d7783e38b7778fdc3a6a91e437522e62676d76a9
Daniël van de Burgt <secretlymexico@gmail.com>
2010-10-24 19:30:37 +0200

    fixed description of browser spec

--- spec/lib/webr/browser_spec.rb
+++ spec/lib/webr/browser_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Webr::Jasmine::Browser do
+describe Webr::Browser do
 
   it "instantiates" do
     browser = Webr::Browser.new
