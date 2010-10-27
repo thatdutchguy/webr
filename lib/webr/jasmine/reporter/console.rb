469b16991d9d41498ae81f1cb51e8c5612f83d53
Daniël van de Burgt <secretlymexico@gmail.com>
2010-10-27 23:16:25 +0200

    a little more console reporter love

--- lib/webr/jasmine/reporter/console.rb
+++ lib/webr/jasmine/reporter/console.rb
@@ -1,11 +1,29 @@
 module Webr::Jasmine::Reporter
   class Console < Webr::Jasmine::Reporter::Base
+    def initialize(jasmine)
+      super
+      
+      @fail_count = 0
+    end
+
+
     def reportRunnerResults(runner)
       super(runner)
       summarize(runner)
     end
 
+    def reportSpecResults(spec)
+      super
+      if spec.results.passed
+        $stdout.write('.')
+      else
+        $stdout.write('F')
+      end
+    end
+
+
     def summarize(runner)
+      puts "\n"
       render_results(runner.topLevelSuites)
       puts "\n"
       render_summary(runner)
@@ -24,9 +42,10 @@ module Webr::Jasmine::Reporter
   
     def render_spec(spec)
       unless spec.results.passed
-        puts "\n"
-        puts "FAILED:"
-        puts spec.getFullName
+        puts "\nFailures:" if @fail_count == 0
+        @fail_count += 1
+        puts "  #{@fail_count}) #{spec.getFullName}"
+        spaces = " " * (4 + @fail_count.to_s.size)
         spec.results.getItems.each do |item|
           unless item.passed
             backtrace = if error = item['error']
@@ -34,8 +53,11 @@ module Webr::Jasmine::Reporter
             else
               item.trace.stack
             end
-            puts item.to_s
-            puts filter_backtrace(backtrace)
+            puts spaces + item.to_s
+            filter_backtrace(backtrace).each_line do |line|
+              puts spaces + line
+            end
+            puts ""
           end
         end
       end
@@ -49,8 +71,8 @@ module Webr::Jasmine::Reporter
       hours, minutes, seconds, fraction = Date.day_fraction_to_time(@finished_at - @started_at)
       time_taken = "%0.8f" % (hours*60*60 + minutes*60 + seconds + fraction.to_f)
 
-      puts "Examples: #{specs.length}, Failure#{'s' unless results.failedCount == 1}: #{results.failedCount}"
       puts "Finished in #{time_taken}s"
+      puts "Examples: #{specs.length}, Failure#{'s' unless results.failedCount == 1}: #{results.failedCount}"
     end
   end
 
