6bafec4d7726f36fe406bef25f7ce08ae5cd39cd
Daniël van de Burgt <secretlymexico@gmail.com>
2011-05-31 23:42:39 -0500

    whoops, specs now runnable by ruby 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-require 'lib/webr'
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'webr'
 
 DATA_PATH = File.expand_path(File.dirname(__FILE__) + '/data')
 EXT_PATH  = File.expand_path(File.dirname(__FILE__) + '/../ext')
